4940b48b5761202be844b7e7df3157f31340daec
Thomas Rawyler <rawyler@gmail.com>
2013-05-21 22:07:29 +0200

    added team model

---
+++ app/models/Team.scala
@@ -0,0 +1,29 @@
+package models
+
+import org.joda.time.DateTime
+import java.util.Locale
+import DatabaseTypeMappers._
+import play.api.db.slick.Config.driver.simple._
+import scala.slick.driver.ExtendedProfile
+import scala.slick.session.Database
+
+case class Team(
+  id: Option[Long] = None,
+  name: String,
+  description: Option[String] = None,
+  noAdmin: Boolean = false,
+  noRoot: Boolean = false,
+  createdAt: DateTime = DateTime.now(),
+  var updatedAt: DateTime = DateTime.now())
+
+object Teams extends Table[Team]("teams") {
+  def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+  def name = column[String]("name", O.NotNull)
+  def description = column[String]("description", O.Nullable)
+  def noAdmin = column[Boolean]("noAdmin", O.NotNull, O.Default(false))
+  def noRoot = column[Boolean]("noRoot", O.NotNull, O.Default(false))
+  def createdAt = column[DateTime]("createdAt", O.NotNull)
+  def updatedAt = column[DateTime]("updatedAt", O.NotNull)
+
+  def * = id.? ~ name ~ description.? ~ noAdmin ~ noRoot ~ createdAt ~ updatedAt <> (Team, Team.unapply _)
+}
