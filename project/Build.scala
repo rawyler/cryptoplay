fc13a5945aa304b88049de6165cd97e2e343e5c4
Thomas Rawyler <rawyler@gmail.com>
2013-05-21 17:31:14 +0200

    set up for slick

--- project/Build.scala
+++ project/Build.scala
@@ -4,18 +4,15 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "cryptopus-play"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appName = "cryptoplay"
+  val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+    "com.typesafe.play" %% "play-slick" % "0.3.2")
+
+  val main = play.Project(appName, appVersion, appDependencies).settings( // Add your own project settings here
   )
-
-
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
-
 }
